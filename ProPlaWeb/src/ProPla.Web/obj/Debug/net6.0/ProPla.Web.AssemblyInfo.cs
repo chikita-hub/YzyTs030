f10b36d851d8b95f5b32c3073d98f8f002e43daa
chikita-hub <username@users.noreply.github.com>
2026-03-11 16:32:47 +0900

    20260311 7

--- ProPlaWeb/src/ProPla.Web/obj/Debug/net6.0/ProPla.Web.AssemblyInfo.cs
+++ ProPlaWeb/src/ProPla.Web/obj/Debug/net6.0/ProPla.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProPla.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57dbcc71ba0b86c6c2940ddfcbde2d060d51ccab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5def731de5491584684087dc895499ec792e2a31")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProPla.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProPla.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
